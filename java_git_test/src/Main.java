8eff8e7c6c48d73eb374878f28c63fc6cf1f61c8
zhangzhipeng <zhangzhipeng@renhuihr.com>
2019-02-25 14:52:45 +0800

    测试push

--- java_git_test/src/Main.java
+++ java_git_test/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //push测试
+        System.out.println("Hello World Idea git!");
     }
 }
